84ca80a3b850c1eb50ab23a1eca37547f26e6502
riaPou <nasiryporya1385@gmail.com>
2025-08-06 16:34:48 +0330

    Implements GitHub login functionality
    Enables GitHub login functionality by adding an onClick handler to the login button, triggering the authentication flow.
    
    Also enforces consistent casing in filenames.

---
+++ src/app/lib/actions/auth.ts
@@ -0,0 +1,9 @@
+'use server'
+import { signIn, signOut } from '@/auth'
+
+export const login = async () => {
+  await signIn('github', { redirectTo: '/dashboard' })
+}
+export const logout = async () => {
+  await signOut({ redirectTo: '/' })
+}
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,9 @@
+'use client'
+import { login } from "@/app/lib/actions/auth";
 export default function Login() {
   return (
     <div className='h-screen flex items-center justify-center'>
-        <button className="bg-neutral-700 p-10 rounded-2xl text-white hover:bg-neutral-800 text-2xl">Login with GitHub </button>
+        <button onClick={()=>login()} className="bg-neutral-700 p-10 rounded-2xl text-white hover:bg-neutral-800 text-2xl">Login with GitHub </button>
     </div>
   )
 }
