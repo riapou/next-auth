1dc5e052ceebfad06b7cfd0a58f5f0a06ad420f5
riaPou <nasiryporya1385@gmail.com>
2025-08-06 16:00:53 +0330

    Sets up basic app structure with Tailwind CSS
    Configures Tailwind CSS, removes boilerplate code and unnecessary dependencies.
    Adds login page link to the home page.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  return (
+    <div className='h-screen flex flex-col items-center p-10 gap-10'>
+      <h1 className='text-3xl font-bold'>welcome to your dashboard</h1>
+      <h1 className='text-2xl font-bold'>name</h1>
+      <h1 className='text-9xl font-bold'>🐳</h1>
+    </div>
+  )
+}
---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+        <button className="bg-neutral-700 p-10 rounded-2xl text-white hover:bg-neutral-800 text-2xl">Login with GitHub </button>
+    </div>
+  )
+}
